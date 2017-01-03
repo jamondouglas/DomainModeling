24ccfc4d693c5c7c7d481a516283d91a0006b57a
Terril Jamon Douglas <jamon@jamon-MBP.local>
2017-01-02 19:58:29 -0800

    Working with Either DATAType

--- src/com/jamondouglas/domainmodeling/model/Account/Account.scala
+++ src/com/jamondouglas/domainmodeling/model/Account/Account.scala
@@ -1,20 +1,40 @@
 package com.jamondouglas.domainmodeling.model.Account
 
-import java.util.Date
+import java.util.{Date, Calendar}
+
+import com.jamondouglas.domainmodeling.model.Account.common.Amount
 
 /**
   * Created by jamon on 12/18/16.
   */
 
-
-trait Account{
+object common {
   type Amount = BigDecimal
-}
+  case class Balance(amt: Amount)
+  def today = Calendar.getInstance.getTime
 
-case class X extends Account
+}
+import common._
+// sealed class can only be extended in same file
+sealed trait Account{
+  def no: String
+  def name: String
+  def dateOpen: Option[Date]
+  def dateClosed: Option[Date]
+  def balance: Balance
+}
+case class CheckingAccount(no: String, name: String, dateOpen:Option[Date], dateClosed:Option[Date], balance: Balance) extends Account
+case class SavingsAccount(no:String, name:String, dateOpen:Option[Date], dateClosed: Option[Date], balance: Balance) extends Account
 
 object Account {
-  type Amount = BigDecimal
-  case class Balance(val amount:Amount)
-  case class Account(no: String, name: String, created:Date, as_of: Option[Long], balance:Balance)
+  //  case class Account(no: String, name: String, created:Date, as_of: Option[Long], balance:Balance)
+  def makeCheckingAccount(no:String,  name:String, dateOpen: Option[Date], dateClosed: Option[Date], balance: Balance): Either[Error,CheckingAccount] = {
+    if(dateClosed.get before dateOpen.getOrElse(today)) Left(new Error("Closing date can't be before openind date"))
+    else Right(CheckingAccount(no, name, dateOpen, dateClosed, balance))
+  }
+
+  def makeSavingAccountt(no:String,  name:String, dateOpen: Option[Date], dateClosed: Option[Date], balance: Balance ): Either[Error,SavingsAccount] = {
+    if(dateClosed.get before dateOpen.getOrElse(today)) Left(new Error("Closing date can't be before openind date"))
+    else Right(SavingsAccount(no, name, dateOpen, dateClosed, balance))
+  }
 }
